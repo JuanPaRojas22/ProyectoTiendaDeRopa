1278a4d8c2c35ea6616d321f0897be4583fe6c8c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-24 19:35:59 +0000

    Commit de prueba 2

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -46,7 +46,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Page Title',
+            'Prueba',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Inter Tight',
                   color: Colors.white,
